09853d8c4ef1539b944e13080a4b38d4e4ffb60c
Danseren <etlendo@mail.ru>
2026-05-20 18:25:03 +0500

    feat: add send_notification Celery task

--- app/workers/celery_app.py
+++ app/workers/celery_app.py
@@ -9,3 +9,9 @@ celery_app.conf.update(
     accept_content=["json"],
     timezone="Europe/Moscow",
 )
+
+
+@celery_app.task(name="app.workers.celery_app.send_notification")
+def send_notification(task_id: int, task_title: str, new_status: str):
+    print(f"[EMAIL] Задача #{task_id} '{task_title}' → {new_status}")
+    return {"status": "sent", "task_id": task_id}
